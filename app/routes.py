f5654515234a31b0dde5f909212931069725ff52
Reetta Valimaki <reetta.valimaki8@gmail.com>
2021-08-07 16:53:19 +0200

    Adding piechart

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,8 @@ from wtforms import SubmitField
 import requests, os
 from werkzeug.wrappers import Response
 import json
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 secret_tokens = json.load(open('secret_tokens.json', 'r'))
 email = secret_tokens['email']
@@ -36,6 +37,52 @@ headers = {
 url = f'{base_url}/v1/projects/'
 r =requests.get(url, headers=headers)
 
+# def merge_dicts(a, b, path = None):
+# 	"merges b into a"
+# 	a = a['value']
+# 	b = b['value']
+# 	print(b)
+# 	if path is None: path = []
+# 	for key in b:
+# 		if key in a:
+# 			if isinstance(a[key], dict) and isinstance(b[key], dict):
+# 				print(key)
+# 				merge(a[key], b[key], path + [str(key)])
+# 			elif a[key] == b[key]:
+# 				print(key)
+# 				pass # same leaf value
+# 			else:
+# 				raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+# 		else:
+# 			print(key)
+# 			a[key] = b[key]
+# 	return a
+
+def count_items(dict, key, value):
+	count = 0
+	for item in dict:
+	    if item[key] == value:
+	        count += 1
+	return count
+
+def charts(submissions_machine):
+	operational_count = count_items(submissions_machine, key = 'operational_mill', value = 'yes')
+	not_operational_count = count_items(submissions_machine, key = 'operational_mill', value = 'no')
+	labels = 'Operational', 'Not operational'
+	colors = ['green', 'red']
+	sizes = [operational_count, not_operational_count]
+	explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+	fig, ax1 = plt.subplots()
+	ax1.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
+	        shadow=True, startangle=90)
+	ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+	if not os.path.exists('app/static/figures'):
+		outdir = os.makedirs('app/static/figures')
+	fig.savefig('app/static/figures/piechart.jpg', bbox_inches='tight')   # save the figure to file
+	plt.close(fig)   
+
+
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
@@ -43,6 +90,9 @@ def index():
 	submissions = odata_submissions(base_url, aut, projectId, formId)
 	submissions_machine = odata_submissions_machine(base_url, aut, projectId, formId)
 	submissions_len = len(submissions.json()['value'])
+	charts(submissions_machine.json()['value'])
+
+	#merged_dict = merge_dicts(submissions.json(), submissions_machine.json())
 	return render_template('index.html', submissions_len = submissions_len, submissions=submissions.json(), submissions_machine = submissions_machine.json(), title='Map')
 
 
@@ -66,3 +116,19 @@ def export_data():
 	response = Response(r.content, content_type='zip')
 	response.headers.set("Content-Disposition", "attachment", filename=f"{file_name}.zip")
 	return response
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
