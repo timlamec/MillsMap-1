fd528f55c5f065f582e318baa61ff0ad7d487132
Reetta Valimaki <reetta.valimaki8@gmail.com>
2021-08-16 09:07:55 +0200

    Adding filtering options

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, send_file, send_from_directory
+from flask import render_template, send_file, send_from_directory, request
 from app import app
-from app.odk_requests import odata_submissions, export_submissions, odata_submissions_machine
+from app.odk_requests import odata_submissions, export_submissions, odata_submissions_machine, odata_submissions_table
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 import requests, os
 from werkzeug.wrappers import Response
 import json
 from app.graphics import count_items, unique_key_counts, charts
+import pandas as pd
+from pandas.io.json import json_normalize
 
 secret_tokens = json.load(open('secret_tokens.json', 'r'))
 email = secret_tokens['email']
@@ -56,27 +58,75 @@ r =requests.get(url, headers=headers)
 # 			print(key)
 # 			a[key] = b[key]
 # 	return a
+submissions = odata_submissions(base_url, aut, projectId, formId)
+submissions_machine = odata_submissions_machine(base_url, aut, projectId, formId)
+submissions_len = len(submissions.json()['value'])
+charts(submissions_machine.json()['value'], submissions.json()['value'])
+submissions_table =  pd.DataFrame(odata_submissions_table(base_url, aut, projectId, formId, 'Submissions')['value'])
+submissions_machine_table =  pd.DataFrame(odata_submissions_table(base_url, aut, projectId, formId, 'Submissions.machines.machine')['value'])
+dictionary_columns = []
+while True:
+	data_types = []
+	for col in submissions_table:
+		data_types.append(type(submissions_table[col][0]))
+	if dict not in data_types:
+		break
+	for col in submissions_table:
+		if (type(submissions_table[col][0])) == dict:
+			dictionary_columns.append(col)
+			submissions_table = pd.concat([submissions_table.drop(col, axis=1), submissions_table[col].apply(pd.Series)], axis=1)
+#submissions_table.to_csv('submissions_table.csv')
 
 
+dictionary_columns = []
+while True:
+	data_types = []
+	for col in submissions_machine_table:
+		data_types.append(type(submissions_machine_table[col][0]))
+	if dict not in data_types:
+		break
+	for col in submissions_machine_table:
+		if (type(submissions_machine_table[col][0])) == dict:
+			dictionary_columns.append(col)
+			submissions_machine_table = pd.concat([submissions_machine_table.drop(col, axis=1), submissions_machine_table[col].apply(pd.Series)], axis=1)
+#submissions_machine_table.to_csv('submissions_machine_table.csv')
 
-
-
-
+submissions_all = submissions_table.merge(submissions_machine_table, left_on = '__id', right_on = '__Submissions-id')
+#submissions_all.to_csv('submissions_all.csv')
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-	submissions = odata_submissions(base_url, aut, projectId, formId)
-	submissions_machine = odata_submissions_machine(base_url, aut, projectId, formId)
-	submissions_len = len(submissions.json()['value'])
-	charts(submissions_machine.json()['value'], submissions.json()['value'])
+	filter_selection_dict = {} 
+	filter_selections = ['mill_owner', 'commodity_milled', 'mill_type', 'operational_mill', 'non_operational', 'energy_source', 'flour_fortified', 'flour_fortified_standard']
+	unique_values_filters = []
+	filter_columns = submissions_all.loc[:,filter_selections]
+	
+	for col in filter_columns:
+		values_list = []
+		values_list = filter_columns.loc[:,col].str.split()
+		unique_values_list =[]
+		for item in (values_list):
+			if type(item) == list:
+				for sub_item in item:
+					if sub_item not in unique_values_list:
+						unique_values_list.append(sub_item)
+			else:
+				if sub_item not in unique_values_list:
+					unique_values_list.append(sub_item)
+		filter_selection_dict[col] = unique_values_list
+		unique_values_filters.append(unique_values_list)
+	return render_template('index.html', filter_selection_dict = filter_selection_dict, submissions_len = submissions_len, submissions=submissions.json(), submissions_machine = submissions_machine.json(), title='Map')
 
-
-
-	#merged_dict = merge_dicts(submissions.json(), submissions_machine.json())
-	return render_template('index.html', submissions_len = submissions_len, submissions=submissions.json(), submissions_machine = submissions_machine.json(), title='Map')
-
+@app.route('/filterform', methods = ['GET', 'POST'])
+def filter_data():
+	print('jee filterform')
+	if request.method == 'POST':
+		choices = request.form
+		print(choices)
+		print('jee FILTERFORM')
+	return render_template('filterform.html', choices = choices)
 
 @app.route('/download_data/')
 def export_data():
@@ -98,20 +148,4 @@ def export_data():
 	response = Response(r.content, content_type='zip')
 	response.headers.set("Content-Disposition", "attachment", filename=f"{file_name}.zip")
 	return response
-
-
-@app.route('/plot.png')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
 
