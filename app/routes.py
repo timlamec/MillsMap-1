a4c53f74d68df4ba960d522659d89de02b42e6d1
ivangayton <ivangayton@gmail.com>
2021-09-03 15:52:24 +0900

    Only fetch mill points, not machines, and flatten dict using generators instead of pandas

--- app/routes.py
+++ app/routes.py
@@ -49,6 +49,32 @@ headers = {
 url = f'{base_url}/v1/projects/'
 r =requests.get(url, headers=headers)
 
+# TODO: move to app.helper_functions
+from collections.abc import MutableMapping
+def _flatten_dict_gen(d, parent_key, sep):
+    """Flatten dictionary using python generators. Probably 100x faster than doing it using Pandas. Code adapted from https://www.freecodecamp.org/news/how-to-flatten-a-dictionary-in-python-in-4-different-ways/"""
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            yield from flatten_dict(v, new_key, sep=sep).items()
+        else:
+            yield new_key, v
+
+def flatten_dict(d: MutableMapping,
+                 parent_key: str = '',
+                 sep: str = '.'):
+    """Flatten dictionary using Python generators"""
+    return dict(_flatten_dict_gen(d, parent_key, sep))
+
+@app.route('/mills')
+def mills():
+    start_time = time.perf_counter()
+    submissions_response = odata_submissions(base_url, aut, projectId, formId)
+    mill_request_complete_time = time.perf_counter()
+    submissions = submissions_response.json()['value']
+    flatsubs = [flatten_dict(sub) for sub in submissions]
+    return json.dumps(flatsubs)
+    
 
 @app.route('/mill_points')
 def mill_points():
@@ -115,7 +141,8 @@ def json_test():
 @app.route('/index')
 @app.route('/home')
 def index():
-	return render_template('index.html', title='Map')
+	return render_template('index.html',
+                               title='Map')
 
 @app.route('/filterform', methods = ['GET', 'POST'])
 def filter_data():
