47888684e56d462cee7924209606b9436c7800bc
ivangayton <ivangayton@gmail.com>
2021-09-03 12:03:05 +0900

    split sidebar toggle from map javascript;

---
+++ app/static/controls.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('hide');
+        $('#mapbar').toggleClass('col-8');
+        $('#left-arrow').toggleClass('hide');
+        $('#right-arrow').toggleClass('hide');
+    });
+
+});
+
+$(document).ready(function () {
+    $('#sidebarOpen').on('click', function () {
+        $('#sidebar').toggleClass('hide');
+        $('#mapbar').toggleClass('col-8');
+        $('#left-arrow').toggleClass('hide');
+        $('#right-arrow').toggleClass('hide');
+    });
+
+});
+</script>
